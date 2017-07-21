4e6f6e4c4d74c4ada95564a4e6a9a17b33482a44
Skyler Rexroad <skyrex1095@gmail.com>
2017-07-20 19:44:42 -0600

    Added title

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 	<div id="app">
+		<h1>DO</h1>
 		<router-view></router-view>
 	</div>
 </template>
@@ -11,13 +12,14 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Cabin');
+@import url('https://fonts.googleapis.com/css?family=Cabin|Cabin+Sketch');
 
 body {
 	background: linear-gradient(to bottom right, #74ebd5, #ACB6E5);
 	min-height: 100vh;
 }
 
+$title-font: 'Cabin Sketch', 'Avenir', Helvetica, Arial, sans-serif;
 $font-family: 'Cabin', 'Avenir', Helvetica, Arial, sans-serif;
 
 $font-dark: #444;
@@ -28,12 +30,21 @@ $font-dark: #444;
 	-moz-osx-font-smoothing: grayscale;
 
 	display: flex;
+	flex-direction: column;
 	align-items: center;
 	justify-content: center;
 
 	min-height: 100vh;
 
 	color: $font-dark;
+
+	h1 {
+		margin: 0;
+
+		font-family: $title-font;
+		font-size: 8rem;
+		color: rgba(#fff, .6);
+	}
 }
 
 input {
